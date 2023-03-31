eaf854ffc670a066a217e0d3d5fb358f98b1b6af
My Phung Tieu <tmp03099@gmail.com>
2023-03-30 23:38:40 -0400

    updated form

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,48 @@
+import ContactForm from "./ContactForm";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+import Accordion from "react-bootstrap/Accordion";
+import { useAccordionButton } from "react-bootstrap/AccordionButton";
+import Card from "react-bootstrap/Card";
+
 function Footer() {
+  const CustomToggle = ({ children, eventKey }) => {
+    const decoratedOnClick = useAccordionButton(eventKey, () =>
+      console.log("totally custom!")
+    );
+
+    return (
+      <button
+        type="button"
+        style={{ backgroundColor: "pink" }}
+        onClick={decoratedOnClick}
+      >
+        {children}
+      </button>
+    );
+  };
   return (
     <section className="flex justify-center items-center">
       <div className="border-4 outline-2 bg-gray-900 shadow-2xl rounded-3xl p-12 text-white text-center">
         <div>
           <h3 className="text-amber-300">Interested in working together?</h3>
-          <h2>STAY CONNECTED</h2>
+          <div className="flex justify-center">
+            <Accordion>
+              <Card>
+                <Card.Header>
+                  <CustomToggle eventKey="0">
+                    <h2 className="mx-4">STAY CONNECTED</h2>
+                    <FontAwesomeIcon icon={faPaperPlane} />
+                  </CustomToggle>
+                </Card.Header>
+                <Accordion.Collapse eventKey="0">
+                  <Card.Body>
+                    <ContactForm />
+                  </Card.Body>
+                </Accordion.Collapse>
+              </Card>
+            </Accordion>
+          </div>
           <p>
             I'm currently looking for the opportunity to join a team of
             developer. If you think I might be a good fit, please contact me.
@@ -17,6 +55,7 @@ function Footer() {
           </div>
         </div>
       </div>
+      <ContactForm />
     </section>
   );
 }
--- src/services/quotes-api.js
+++ src/services/quotes-api.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 export const getQuotes = async () => {
-  // try {
-  //   const respone = await axios.get("https://animechan.vercel.app/api/random");
-  //   console.log(respone);
-  //   const data = respone.data;
-  //   console.log(data);
-  //   return data;
-  // } catch (error) {
-  //   console.error(error);
-  // }
+  try {
+    const respone = await axios.get("https://animechan.vercel.app/api/random");
+    console.log(respone);
+    const data = respone.data;
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
 };
